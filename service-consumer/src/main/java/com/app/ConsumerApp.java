7aba82b00d4af57b826cf0e1a45602bf4cf4b833
dailele <kunting007@163.com>
2021-02-23 16:44:54 +0800

    modify

--- service-consumer/src/main/java/com/app/ConsumerApp.java
+++ service-consumer/src/main/java/com/app/ConsumerApp.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.hystrix.EnableHystrix;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
@@ -11,6 +12,7 @@ import org.springframework.web.client.RestTemplate;
 @EnableFeignClients
 @SpringBootApplication
 @EnableEurekaClient
+@EnableHystrix
 public class ConsumerApp {
 
     public static void main(String[] args) {
--- service-consumer/src/main/java/com/app/ConsumerService.java
+++ service-consumer/src/main/java/com/app/ConsumerService.java
@@ -1,5 +1,7 @@
 package com.app;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixProperty;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -36,4 +38,29 @@ public class ConsumerService {
         String json = feignClient.call();
         return json;
     }
+
+
+    /**
+     * 客户端调用时使用断路器
+     * @return
+     */
+    @HystrixCommand(fallbackMethod = "dealErr",commandProperties = {
+            @HystrixProperty(name="circuitBreaker.enabled",value = "true"),//开启断路器
+            @HystrixProperty(name="circuitBreaker.requestVolumeThreshold",value = "2"),//打开断路器的单位时间内最小请求数
+            @HystrixProperty(name="circuitBreaker.sleepWindowInMilliseconds",value = "1000"), //开启断路器的单位时间
+            @HystrixProperty(name="circuitBreaker.errorThresholdPercentage",value = "60")//开启断路器的最小失败率60%
+    })
+    @ResponseBody
+    @RequestMapping("/index_hys")
+    public String router3() {
+        // 根据应用名称调用服务
+        String json = restTemplate.getForObject(
+                "http://" + providerName + "/info", String.class);
+        return json;
+    }
+
+    public String dealErr(){
+        return "Hystrix 熔断";
+    }
+
 }
