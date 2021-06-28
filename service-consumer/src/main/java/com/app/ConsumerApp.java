391dbecd60fb2c17a94e2fb3df86d7f35f66ce4f
dailele <kunting007@163.com>
2021-06-28 10:50:02 +0800

    modify

--- service-consumer/src/main/java/com/app/ConsumerApp.java
+++ service-consumer/src/main/java/com/app/ConsumerApp.java
@@ -2,6 +2,7 @@ package com.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.hystrix.EnableHystrix;
@@ -13,6 +14,7 @@ import org.springframework.web.client.RestTemplate;
 @SpringBootApplication
 @EnableEurekaClient
 @EnableHystrix
+@EnableDiscoveryClient
 public class ConsumerApp {
 
     public static void main(String[] args) {
